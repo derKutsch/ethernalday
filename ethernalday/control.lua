fc9d72e1f6d99a8e9d4f3623c0c39c9d1ad332ac
Patrick <p.kucera@me.com>
2022-12-04 15:19:43 +0100

    renamed and add functionality
    renamed folder structure
    added on_init back in (compatibility issues with existing worlds should be resolved)

--- ethernalday/control.lua
+++ ethernalday/control.lua
@@ -1,4 +1,10 @@
 require "util"
+script.on_init(function() On_Init() end)
+
+function On_Init()
+        game.surfaces[1].always_day = true
+        game.surfaces[1].daytime = 1.0
+end
 
 script.on_event(defines.events.on_player_created,function ()
         game.surfaces[1].always_day = true
--- ethernalnight/control.lua
+++ ethernalnight/control.lua
@@ -1,5 +1,9 @@
 require "util"
 
+function On_Init()
+        game.surfaces[1].freeze_daytime = true
+        game.surfaces[1].daytime = 0.5
+end
 
 script.on_event(defines.events.on_player_created,function ()
         game.surfaces[1].freeze_daytime = true
